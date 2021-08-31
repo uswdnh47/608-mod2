d4f645ffc428e78e4f83a4c2c3dfa36b5ea5535b
Deshawn Hunter <89186945+uswdnh47@users.noreply.github.com>
2021-08-30 22:01:12 -0500

    Update central-with-statistics.py

--- central-with-statistics.py
+++ central-with-statistics.py
@@ -1,1 +1,8 @@
+import statistics as stats
+values = [47,95,88,73,88,84]
+print (len(values))
+print (sum(values))
+print (stats.mean(values))
+print (stats.median(values))
+print (stats.mode(values))      
 
