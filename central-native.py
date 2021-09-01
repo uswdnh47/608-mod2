d4f0cf80b1c4abd76e9cee2263bb41d0c6896807
Deshawn Hunter <89186945+uswdnh47@users.noreply.github.com>
2021-08-31 20:12:19 -0500

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -1,8 +1,26 @@
 values = [47,95,88,73,88,84]
-print (len(values))
-print (sum(values))
-print (sum(values)/len(values))
-print (len(values))
-print (len(values))      
+
+print (len(values)) #count
+print (sum(values)) #sum
+
+#mean
+mean = (sum(values)/len(values))
+print (mean)
+
+#median
+n = len(values)
+values.sort()
+
+if n % 2 == 0:
+    median1 = values[n//2]
+    median2 = values[n//2-1]
+    median = (median1 + median2)/2
+else:
+    median = values[n//2]
+print (median)
+
+#mode
+mode = max(values, key = values.count)
+print (mode)  
 
 
